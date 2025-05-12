bdd7ca47782437bff7053982e9e555d75188458b
Andressa Fontes <andressafc477@gmail.com>
2025-05-11 21:10:53 -0300

    first commit

---
+++ Projeto_ClassLibrary.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Projeto_ClassLibrary.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Avanade")]
+[assembly: AssemblyProduct("Projeto_ClassLibrary.Tests")]
+[assembly: AssemblyCopyright("Copyright © Avanade 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1b9149b6-0b8d-4721-ad86-4ea41889063c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Projeto_ClassLibrary.Tests/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MinhaBiblioteca;
+
+namespace Projeto_ClassLibrary.Tests
+{
+    [TestClass]
+    public class MinhaClasseTests
+    {
+        [TestMethod]
+        public void Saudacao_DeveRetornarMensagemCorreta_QuandoNomeForValido()
+        {
+            // Arrange
+            var minhaClasse = new MinhaClasse();
+            var nome = "João";
+            var esperado = "Olá, João!";
+
+            // Act
+            var resultado = minhaClasse.Saudacao(nome);
+
+            // Assert
+            Assert.AreEqual(esperado, resultado);
+        }
+
+        [TestMethod]
+        public void Saudacao_DeveRetornarMensagemCorreta_QuandoNomeForVazio()
+        {
+            // Arrange
+            var minhaClasse = new MinhaClasse();
+            var nome = string.Empty;
+            var esperado = "Olá, !";
+
+            // Act
+            var resultado = minhaClasse.Saudacao(nome);
+
+            // Assert
+            Assert.AreEqual(esperado, resultado);
+        }
+
+        [TestMethod]
+        public void Saudacao_DeveRetornarMensagemCorreta_QuandoNomeForNulo()
+        {
+            // Arrange
+            var minhaClasse = new MinhaClasse();
+            string nome = null;
+            var esperado = "Olá, !";
+
+            // Act
+            var resultado = minhaClasse.Saudacao(nome);
+
+            // Assert
+            Assert.AreEqual(esperado, resultado);
+        }
+    }
+}
